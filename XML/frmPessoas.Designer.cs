31c4b7717f6ecd89b66038843513e5c2acbd82a8
Ronaldo <Ronaldo@ESCRITORIO2>
2016-08-19 19:21:00 -0300

    Add project files.

---
+++ XML/frmPessoas.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace XML
+{
+    partial class frmPessoas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPessoas = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPessoas
+            // 
+            this.dataGridViewPessoas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPessoas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nome,
+            this.Telefone});
+            this.dataGridViewPessoas.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewPessoas.Name = "dataGridViewPessoas";
+            this.dataGridViewPessoas.Size = new System.Drawing.Size(886, 493);
+            this.dataGridViewPessoas.TabIndex = 0;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Código";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            // 
+            // frmPessoas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 635);
+            this.Controls.Add(this.dataGridViewPessoas);
+            this.Name = "frmPessoas";
+            this.Text = "XML Pessoas";
+            this.Load += new System.EventHandler(this.frmPessoas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPessoas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPessoas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+    }
+}
+
---
+++ XML/frmPessoas.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XML
+{
+    class Pessoa
+    {
+        #region Atributos
+        private int codigo;
+        private string nome;
+        private string telefone;
+        #endregion
+
+        #region Propriedades
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+        #endregion
+
+        #region Métodos
+        //Aqui ficarão os métodos
+        public static List<Pessoa> ListarPessoas()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            XElement xml = XElement.Load("Pessoas.xml");
+            foreach (XElement x in xml.Elements())
+            {
+                Pessoa p = new Pessoa()
+                {
+                    codigo = int.Parse(x.Attribute("codigo").Value),
+                    nome = x.Attribute("nome").Value,
+                    telefone = x.Attribute("telefone").Value
+                };
+                pessoas.Add(p);
+            }
+            return pessoas;
+        }
+
+        #endregion
+    }
+
+    public partial class frmPessoas : Form
+    {
+        public frmPessoas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPessoas_Load(object sender, EventArgs e)
+        {
+            //pessoas = Pessoa.ListarPessoas();
+            //gridView.DataSource = pessoas;
+        }
+    }
+}
