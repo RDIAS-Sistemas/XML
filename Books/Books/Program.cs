a9cede904f24ee94209e3ba799beb665024017fc
RonaldoFreitasDias <ronaldofreitasdias@live.com>
2016-08-19 22:32:08 -0300

    Alex

--- Books/Books/Program.cs
+++ Books/Books/Program.cs
@@ -11,16 +11,29 @@ namespace Books
     {
         static void Main(string[] args)
         {
-            XmlTextReader reader = new XmlTextReader("books.xml");
+            int i = 0;
+            string nomeElemento = "";
+            double valorBruto = 0.0;
+
+            XmlTextReader reader = new XmlTextReader("santour.xml");
             while (reader.Read())
             {
                 switch (reader.NodeType)
                 {
                     case XmlNodeType.Element: // The node is an element.
-                        Console.Write("<" + reader.Name);
+                        nomeElemento = reader.Name;
+                        Console.Write("<" + nomeElemento);
+
+                        while (reader.MoveToNextAttribute()) // Read the attributes.
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
                         Console.WriteLine(">");
                         break;
                     case XmlNodeType.Text: //Display the text in each element.
+                        if (nomeElemento == "valorBruto")
+                        {
+                            ++i;
+                            valorBruto = Convert.ToDouble(reader.Value);
+                        }
                         Console.WriteLine(reader.Value);
                         break;
                     case XmlNodeType.EndElement: //Display the end of the element.
@@ -29,7 +42,8 @@ namespace Books
                         break;
                 }
             }
-        Console.ReadLine();
+
+            Console.ReadLine();
         }
     }
 }
