6ecec53ddb24f5e7048ca643aa90b417568868e4
RonaldoFreitasDias <ronaldofreitasdias@live.com>
2016-08-19 23:23:07 -0300

    Alex

--- Books/Books/Program.cs
+++ Books/Books/Program.cs
@@ -4,14 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Books
 {
     class Program
     {
+        public static double StringToOtherTypes(string stringVal)
+        {
+            if (stringVal.Contains("."))
+                stringVal = stringVal.Replace(".", ",");
+
+            decimal decval = System.Convert.ToDecimal(stringVal);
+            double doubleval = System.Convert.ToDouble(stringVal);
+            float fval = System.Convert.ToSingle(stringVal);
+
+            return doubleval;
+        }
+
         static void Main(string[] args)
         {
-            int i = 0;
+            int qtd = 0;
             string nomeElemento = "";
             double valorBruto = 0.0;
 
@@ -22,27 +35,29 @@ namespace Books
                 {
                     case XmlNodeType.Element: // The node is an element.
                         nomeElemento = reader.Name;
-                        Console.Write("<" + nomeElemento);
-
+                        //Console.Write("<" + nomeElemento);
+                    
                         while (reader.MoveToNextAttribute()) // Read the attributes.
                             Console.Write(" " + reader.Name + "='" + reader.Value + "'");
-                        Console.WriteLine(">");
+                        //Console.WriteLine(">");
                         break;
                     case XmlNodeType.Text: //Display the text in each element.
                         if (nomeElemento == "valorBruto")
                         {
-                            ++i;
-                            valorBruto = Convert.ToDouble(reader.Value);
+                            ++qtd;
+                            valorBruto = valorBruto + StringToOtherTypes(reader.Value);
                         }
-                        Console.WriteLine(reader.Value);
+                        //Console.WriteLine(reader.Value);
                         break;
                     case XmlNodeType.EndElement: //Display the end of the element.
-                        Console.Write("</" + reader.Name);
-                        Console.WriteLine(">");
+                        //Console.Write("</" + reader.Name);
+                        //Console.WriteLine(">");
                         break;
                 }
             }
 
+            Console.WriteLine("A quantidade de transações foi de " + qtd);
+            Console.WriteLine("O valor total das transações é de R$ " + string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", valorBruto));
             Console.ReadLine();
         }
     }
